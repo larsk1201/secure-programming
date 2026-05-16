fe0610396b13e1d91d42ba45e5963d6fade4e1a9
Mart-Velema <mart.velema@student.nhlstenden.com>
2026-05-16 22:37:09 +0200

    Implemented proper back button behaviour

--- app/src/main/java/com/nhlstenden/guineatrade/activities/MainActivity.kt
+++ app/src/main/java/com/nhlstenden/guineatrade/activities/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.nhlstenden.guineatrade.activities
 
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
 import android.widget.TextView
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -42,6 +45,16 @@ class MainActivity : AppCompatActivity() {
                     1 -> R.id.nav_store
                     2 -> R.id.nav_inventory
                     else -> R.id.nav_home
+                }
+            }
+        })
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (viewPager.currentItem == 0) {
+                    finish()
+                } else {
+                    viewPager.currentItem = 0
                 }
             }
         })
