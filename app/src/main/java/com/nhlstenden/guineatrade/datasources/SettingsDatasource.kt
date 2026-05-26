abc23db2192b6783b19c8159858a061b72c42982
Mart-Velema <mart.velema@student.nhlstenden.com>
2026-05-27 00:22:51 +0200

    Fixed datastore not working acros different activities

--- app/src/main/java/com/nhlstenden/guineatrade/datasources/SettingsDatasource.kt
+++ app/src/main/java/com/nhlstenden/guineatrade/datasources/SettingsDatasource.kt
@@ -1,34 +1,30 @@
 package com.nhlstenden.guineatrade.datasources
 
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.preferencesDataStoreFile
+import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 
+val Context.dataStore by preferencesDataStore("user_prefs")
+
 @Singleton
 class SettingsDatasource @Inject constructor(
     @ApplicationContext private val context: Context
 ){
-    private val dataStore: DataStore<Preferences> = PreferenceDataStoreFactory.create {
-        context.preferencesDataStoreFile("user_prefs")
-    }
-
     suspend fun <T> save(key: SettingsKey<T>, value: T) {
-        this.dataStore.edit {
+        this.context.dataStore.edit {
             it[key.key] = value
         }
     }
 
-    suspend fun <T> load(key: SettingsKey<T>, default: T): T? {
-        return this.dataStore.data.map {
+    suspend fun <T> load(key: SettingsKey<T>, default: T): T {
+        return this.context.dataStore.data.map {
             it[key.key]
         }.first() ?: default
     }
