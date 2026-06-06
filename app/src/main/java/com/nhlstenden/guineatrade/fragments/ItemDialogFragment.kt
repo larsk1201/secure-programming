11d67dae3a8ccf0f7e30ac076ff217d5154a9f10
Mart-Velema <mart.velema@student.nhlstenden.com>
2026-06-06 16:58:53 +0200

    Reworked layout, implemented button functionality

--- app/src/main/java/com/nhlstenden/guineatrade/fragments/ItemDialogFragment.kt
+++ app/src/main/java/com/nhlstenden/guineatrade/fragments/ItemDialogFragment.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.DialogFragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.nhlstenden.guineatrade.R
 
 class ItemDialogFragment(val gridViewModel: GridViewModel): DialogFragment() {
@@ -20,6 +25,26 @@ class ItemDialogFragment(val gridViewModel: GridViewModel): DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val weaponText = view.findViewById<TextView>(R.id.item_popup_name)
+        val weaponIcon = view.findViewById<ImageView>(R.id.item_popup_icon)
+
+        weaponText.text = gridViewModel.itemName
+
+        Glide.with(requireContext())
+            .load(gridViewModel.imageUrl)
+            .placeholder(R.drawable.item_not_found)
+            .error(R.drawable.item_not_found)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .into(weaponIcon)
+
+        view.findViewById<Button>(R.id.item_popup_ok).setOnClickListener {
+            this@ItemDialogFragment.dismiss()
+        }
+
+        view.findViewById<Button>(R.id.item_popup_cancel).setOnClickListener {
+            this@ItemDialogFragment.dismiss()
+        }
     }
 
     override fun onStart() {
