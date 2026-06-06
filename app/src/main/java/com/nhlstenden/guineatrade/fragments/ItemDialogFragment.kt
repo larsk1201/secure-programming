612202da06c1861891bd1b27b88a09c789ab875c
Mart-Velema <mart.velema@student.nhlstenden.com>
2026-06-06 16:38:14 +0200

    Fixed popup width

--- app/src/main/java/com/nhlstenden/guineatrade/fragments/ItemDialogFragment.kt
+++ app/src/main/java/com/nhlstenden/guineatrade/fragments/ItemDialogFragment.kt
@@ -1,21 +1,34 @@
 package com.nhlstenden.guineatrade.fragments
 
-import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
-import androidx.appcompat.app.AlertDialog
-import androidx.core.content.getSystemService
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.nhlstenden.guineatrade.R
 
 class ItemDialogFragment(val gridViewModel: GridViewModel): DialogFragment() {
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val inflater = context?.getSystemService<LayoutInflater>()
-        var view = inflater?.inflate(R.layout.popup_item_details, null)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.popup_item_details, container, false)
+    }
 
-        return AlertDialog.Builder(requireContext())
-            .setView(R.layout.popup_item_details)
-            .create()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val dialog = getDialog()
+        if (dialog != null) {
+            val width = ViewGroup.LayoutParams.MATCH_PARENT
+            val height = ViewGroup.LayoutParams.MATCH_PARENT
+            dialog.window!!.setLayout(width, height)
+        }
     }
 }
